60a8c6f62ae32e74967f47a6b2c4db1ff14a7d9d
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2020-12-07 16:41:28 -0500

    Update SlopeIntercept.java

--- SlopeIntercept.java
+++ SlopeIntercept.java
@@ -4,18 +4,37 @@ import java.util.Scanner;
       public static void main(String[] args) {
       Scanner userInput = new Scanner(System.in);
       String name = "";
-      int x1 = 0;
-      int x2 = 0;
-      int y1 = 0;
-      int y2 = 0;
-      int slope = ((y2 - y1)/(x2 - x1));
-      int y1intercept = y1 - slope * x1;
-      int x1intercept = -1 * (y1intercept / slope);
-         {//Prompt the user for input
+      double x1 = 0;
+      double x2 = 0;
+      double y1 = 0;
+      double y2 = 0;
+      //int slope = ((y2 - y1)/(x2 - x1));
+      //int y1intercept = y1 - slope * x1;
+      //int x1intercept = -1 * (y1intercept / slope);
+         {//Prompt the user for starting point coordinates
          System.out.print("Enter the X and Y coordinates of starting point:\n\tx1 = ");
-         x1 = userInput.nextInt();
+         x1 = userInput.nextDouble();
          System.out.print("\ty1 = ");
-         y1 = userInput.nextInt();
+         y1 = userInput.nextDouble();
          }
+         {//Promt the user for ending point coordinates
+         System.out.print("Enter the X and Y coordinates of ending point:\n\tx2 = ");
+         x2 = userInput.nextDouble();
+         System.out.print("\ty2 = ");
+         y2 = userInput.nextDouble();
+         }
+         double slope = ((y2 - y1)/(x2 - x1));
+         double y1intercept = (y1 - slope * x1);
+         double x1intercept = (-1 * (y1intercept / slope));
+         if (x1 == x2);
+            System.out.print("Slope: \"undefined\"");
+         if (y1 == y2);
+            System.out.print("Slope: " + slope +
+            "\nY Intercept: " + y1intercept +
+            "\nX Intercept: \"undefined\"");            
+         else ;
+            System.out.print("Slope: " + slope +
+            "\nY Intercept: " + y1intercept +
+            "\nX Intercept: " + x1intercept);
       }
    }

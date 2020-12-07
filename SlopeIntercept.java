b73969ef8bab2479cd67c89c5a2d1753b68c8c03
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2020-12-07 16:49:53 -0500

    Update SlopeIntercept.java

--- SlopeIntercept.java
+++ SlopeIntercept.java
@@ -26,17 +26,16 @@ import java.util.Scanner;
          double slope = ((y2 - y1)/(x2 - x1));
          double y1intercept = (y1 - slope * x1);
          double x1intercept = (-1 * (y1intercept / slope));
-         {if (x1 == x2);
+         if (x1 == x2)
             System.out.print("Slope: \"undefined\"");
-         }
-         {if (y1 == y2);
-            System.out.print("Slope: " + slope +
-            "\nY Intercept: " + y1intercept +
-            "\nX Intercept: \"undefined\"");            
-         else;
-            System.out.print("Slope: " + slope +
-            "\nY Intercept: " + y1intercept +
-            "\nX Intercept: " + x1intercept);
+         else
+            if (y1 == y2)
+               System.out.print("Slope: " + slope +
+               "\nY Intercept: " + y1intercept +
+               "\nX Intercept: \"undefined\"");            
+            else
+               System.out.print("Slope: " + slope +
+               "\nY Intercept: " + y1intercept +
+               "\nX Intercept: " + x1intercept);
          }
       }
-   }
